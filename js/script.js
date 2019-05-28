a0d996a69684caa8e3661b61411668c32068bd98
Trevor Burnham <tburnham@hubspot.com>
2019-05-27 22:22:05 -0400

    Improve the new site experience in mobile browsers

--- js/script.js
+++ js/script.js
@@ -1,55 +1,60 @@
 // Add "More" controls
 const scrollLinkTemplate = document.getElementById("scroll-link-template");
 const namedAnchors = document.querySelectorAll("a[name]");
-document
-  .querySelectorAll("header, section:not(:last-of-type)")
-  .forEach((section, i) => {
-    const link = document.importNode(scrollLinkTemplate.content, true)
-      .children[0];
-    link.href = `#${namedAnchors[i].name}`;
-    section.appendChild(link);
-  });
+document.querySelectorAll("header, section.bio").forEach((section, i) => {
+  const link = document.importNode(scrollLinkTemplate.content, true)
+    .children[0];
+  link.href = `#${namedAnchors[i].name}`;
+  section.appendChild(link);
+});
 
 // Smooth scrolling
+let smoothScrollFrame;
 const smoothScrollDelta = 0.9;
 document.querySelectorAll('[href*="#"]').forEach(link => {
   link.addEventListener("click", evt => {
     evt.preventDefault();
 
     const moveCloser = () => {
-      const currentPos = document.documentElement.scrollTop;
+      const currentPos = document.scrollingElement.scrollTop;
       const targetPos = document.querySelector(
         `[name=${link.getAttribute("href").slice(1)}]`
       ).offsetTop;
-      if (Math.abs(targetPos - currentPos) < 3) {
-        document.documentElement.scrollTop = targetPos;
+      if (Math.abs(targetPos - currentPos) < 5) {
+        document.scrollingElement.scrollTop = targetPos;
         history.replaceState(null, null, " ");
       } else {
-        document.documentElement.scrollTop = Math.ceil(
+        document.scrollingElement.scrollTop = Math.ceil(
           smoothScrollDelta * currentPos + (1 - smoothScrollDelta) * targetPos
         );
-        requestAnimationFrame(moveCloser);
+        smoothScrollFrame = requestAnimationFrame(moveCloser);
       }
     };
     moveCloser();
   });
 });
+document.addEventListener("mousewheel", () => {
+  cancelAnimationFrame(smoothScrollFrame);
+});
 
 // Fade on scroll
-let fadeOnScrollElements = document.querySelectorAll(".fade-on-scroll");
+const fadeOnScrollElements = document.querySelectorAll(".fade-on-scroll");
+const addHiddenClass = evt => {
+  evt.target.classList.add("hidden");
+  evt.target.removeEventListener("transitionend", addHiddenClass);
+};
 const handleScroll = () => {
   requestAnimationFrame(() => {
     fadeOnScrollElements.forEach(el => {
-      if (el.getBoundingClientRect().top < window.innerHeight / 2) {
+      const pastFadeThreshold = el.getBoundingClientRect().top < 64;
+      if (pastFadeThreshold && !el.classList.contains("fade-out")) {
         el.classList.add("fade-out");
-        el.addEventListener("transitionend", () => {
-          el.classList.add("hidden");
-        });
+        el.addEventListener("transitionend", addHiddenClass);
+      } else if (!pastFadeThreshold && el.classList.contains("fade-out")) {
+        el.classList.remove("hidden", "fade-out");
+        el.removeEventListener("transitionend", addHiddenClass);
       }
     });
-    fadeOnScrollElements = document.querySelectorAll(
-      ".fade-on-scroll:not(.fade-out)"
-    );
   });
 };
 document.addEventListener("scroll", handleScroll, { passive: true });
